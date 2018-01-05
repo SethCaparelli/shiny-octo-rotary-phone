92544ca8820c4b27583f5012105904f0474fb685
Seth <Seth@Seths-iMac.local>
2018-01-05 11:05:37 -0700

    Personal website added

---
+++ personal-website/index.js
@@ -0,0 +1,38 @@
+function onHover() {
+    $("#menuImg").attr('src', 
+'assets/images/IMG_5705.jpg');
+}
+
+function offHover() {
+    $("#menuImg").attr('src', 
+'assets/images/profile-pic.jpg');
+}
+
+
+
+// var photos = ["'assets/images/cactus.jpg'", "'assets/images/reclaim-tree.jpg'", "'assets/images/cactus3.jpg'"];
+
+// var randomIndex = Math.floor(Math.random() * 3);
+
+// var randomPhoto = photos[randomIndex];
+
+// function onHove() {
+//     $("#about-img").attr('src', 
+// randomPhoto);
+// }
+
+// function offHove() {
+//     $("#about-img").attr('src', 
+//     randomPhoto);
+// }
+
+
+function onHove() {
+    $("#about-img").attr('src', 
+'assets/images/cactus3.jpg');
+}
+
+function offHove() {
+    $("#about-img").attr('src', 
+    'assets/images/reclaim-tree.jpg');
+}
